ab65e69ba9f36801a0af2cf137921336b101b758
MaxJevon <32112163+cryptorich1@users.noreply.github.com>
2023-07-06 21:41:04 +0100

    Update 8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -7,6 +7,7 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable [[float], float]:
-  def fn(n: float):
+  return lambda x: x * multiplier
+    """def fn(n: float):
     return n * multiplier
-  return fn
+  return fn """
