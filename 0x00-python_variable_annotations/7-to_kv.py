86ee661e187b53f955bbfe8aed43c9f01f09550f
MaxJevon <32112163+cryptorich1@users.noreply.github.com>
2023-07-06 21:34:31 +0100

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,5 +6,6 @@
         should be annotated as a float. """
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-  return (k, v * v)
+  return (k, float(v**2))
