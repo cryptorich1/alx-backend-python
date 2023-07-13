ef9dd3e68f12e7105a6658d9b66a81b333c4fa63
MaxJevon <32112163+cryptorich1@users.noreply.github.com>
2023-07-13 02:32:58 +0100

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
-import asyncio
-import random
+""" Async Comprehensions """
 
-async def async_generator():
+from asyncio import sleep
+from random import uniform
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """ Async Generator """
     for _ in range(10):
-        await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        await sleep(1)
+        yield uniform(0, 10)
